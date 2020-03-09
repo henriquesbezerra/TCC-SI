56492aeff1b1c6772d7b03d62fc9c2b16cefa7c8
HenriRique <henriquesb92@gmail.com>
2020-03-09 12:06:02 -0300

    Primeira Parte do Painel

---
+++ front-end/src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header(){
+    return (
+        <Container>
+            <h1>Gestão Projeto</h1>
+        </Container>
+    )
+}
